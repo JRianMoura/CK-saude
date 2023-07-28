61dc23658880ee393d2d43240127800a2bc613c3
Judson Rian <judson_@outlook.com>
2023-07-27 22:58:56 -0300

    configuração do touch v-02

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 AOS.init();
 
+function iniciarAnimacao() {
+    var box = document.querySelector(".products-cards, .card-sale");
+    box.classList.add("sale-card:hover, card-inner");
+    setTimeout(function() {
+        box.classList.remove("sale-card:hover, card-inner");
+    }, 1000); 
+}
